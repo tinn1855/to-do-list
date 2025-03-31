e2acbf82849b3894663146bbad13ca451d4b9f80
tinnguyen <tinn1855@gmail.com>
2025-03-31 10:05:55 +0700

    fix add task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,36 @@
 let inputBox = document.getElementById("input-box");
-let listTasks = document.getElementById("listTasks");
+let tableTasks = document.getElementById("tableTasks");
 
-function addTask() {
-  if (inputBox.value === "") {
-    alert("Vui lòng nhập công việc");
-  } else {
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
-        <td>
+const listTasks = [
+  {
+    id: 1,
+    description: "Quét nhà",
+    status: "incomplete",
+  },
+  {
+    id: 2,
+    description: "Rửa chén",
+    status: "completed",
+  },
+];
+
+function renderTasks() {
+  tableTasks.innerHTML = "";
+  listTasks.map((task) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
+       <td>
             <input type="checkbox" name="" id="" />
         </td>
-        <td>${inputBox.value}</td>
+        <td>${task.description}</td>
         <td>
-            <select name="" id="">
-            <option class="completed" value="">Hoàn thành</option>
-            <option class="incomplete" selected value="">
-                Chưa hoàn thành
-            </option>
+            <select class="${task.status}">
+              <option value="incomplete" ${
+                task.status === "incomplete" ? "selected" : ""
+              } >Chưa hoàn thành</option>
+              <option value="completed" ${
+                task.status === "completed" ? "selected" : ""
+              } >Hoàn thành</option>
             </select>
         </td>
         <td>
@@ -24,7 +38,45 @@ function addTask() {
             <button class="btn-delete">Xóa</button>
         </td>
     `;
-    document.getElementById("listTasks").appendChild(tr);
+    tableTasks.appendChild(row);
     inputBox.value = "";
-  }
+  });
 }
+renderTasks();
+
+function addTask() {
+  let tasks = inputBox.value.trim();
+  if (tasks === "") {
+    alert("Vui lòng nhập công việc!");
+    return;
+  }
+
+  let checkTask = listTasks.some(
+    (task) => task.description.toLowerCase() === tasks.toLowerCase()
+  );
+  if (checkTask) {
+    let comfirmTask = confirm(
+      "Công việc đã có trong danh sách. Bạn có chắc chắc muốn thêm không?"
+    );
+    if (!comfirmTask) {
+      return;
+    }
+  }
+
+  let newTask = {
+    // Tạo id tự động cho mỗi công việc khi thêm vào listTasks
+    id: listTasks.length ? listTasks[listTasks.length - 1].id + 1 : 1,
+    description: tasks,
+    status: "incomplete",
+  };
+
+  listTasks.push(newTask);
+  inputBox.value = "";
+  renderTasks();
+}
+
+inputBox.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
